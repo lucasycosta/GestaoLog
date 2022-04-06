cbbf2c03314a83eebe2ae51a050cf1df52787567
Lucas Yamaguti <lucasycosta@gmail.com>
2022-04-06 15:39:17 -0300

    n sei

--- src/test/java/com/zeussh/gestaoLog/apitest/RegistroAPITest.java
+++ src/test/java/com/zeussh/gestaoLog/apitest/RegistroAPITest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 import com.zeussh.gestaoLog.domain.Registro;
 
-import io.cucumber.java.es.Dado;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
